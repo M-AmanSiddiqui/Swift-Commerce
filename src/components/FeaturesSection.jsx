f2c0e0186ffc4059e4f92f4254db1a71552793c6
M-Aman Siddiqui <m.amansiddiqui2024@gmail.com>
2024-11-15 01:47:36 -0800

    bg-add

--- src/components/FeaturesSection.jsx
+++ src/components/FeaturesSection.jsx
@@ -3,105 +3,111 @@ import { IoIosStar } from "react-icons/io";
 import { FaStarHalf } from "react-icons/fa";
 export default function FeaturesSection() {
   return (
-    <div className=' mt-32' >
+    <div className=' mt-32 ' >
+
+      {/* {New Arrivals} */}
+      <div>
 <h1 className='text-4xl font-bold text-center'>NEW ARRIVAL</h1>
-
-
-
-<div className='mt-12 flex pl-14 pr-14 gap-6 '>
-<img className='rounded-lg ' src="/src/assets/imges/Feature img 1.png" alt="ti-shirt" />
-<img className='rounded-lg ' src="/src/assets/imges/Feature img 2.png" alt="ti-shirt" />
-<img className='rounded-lg 'src="/src/assets/imges/Feature img 3.png" alt="ti-shirt" />
-<img className='rounded-lg 'src="/src/assets/imges/Feature img 4.png" alt="ti-shirt" />
-
 </div>
 
+<div className='flex'>
 
-<div className='flex gap-32 ml-14'>
+<div className='mt-12  pl-14 pr-14  '>
+<img className='rounded-lg ' src="/src/assets/images/Feature img 1.png" alt="ti-shirt" />
 <h4 className='font-bold mt-2'>T-Shirt with Tape Details</h4>
-<h4 className='font-bold mt-2 ml-5'>Skinny Fit Jeans</h4>
-<h4 className='font-bold mt-2 ml-16'> Checkered Shirt </h4>
-<h4 className='font-bold mt-2 ml-20'> Sleeve Striped T-shirt</h4>
+<p className='flex flex-wrap text-yellow-300 mt-1 '><IoIosStar /><IoIosStar /><IoIosStar /><IoIosStar /><FaStarHalf /></p>
+<h1 className='mt-2 ml-1 font-satoshi text-1xl'>$120</h1>
 </div>
 
 
-<div className='flex gap-60 ml-6'>
-
-<div className='flex flex-wrap text-yellow-300 mt-1 ml-7'><IoIosStar /><IoIosStar /><IoIosStar /><IoIosStar /><FaStarHalf /><p></p></div>
-<div className='flex flex-wrap text-yellow-300 mt-1 ml-4'><IoIosStar /><IoIosStar /><IoIosStar /><IoIosStar /><FaStarHalf /></div>
-<div className='flex flex-wrap text-yellow-300 mt-1 '><IoIosStar /><IoIosStar /><IoIosStar /><IoIosStar /><FaStarHalf /></div>
-
-<div className='flex flex-wrap text-yellow-300 mt-1 '><IoIosStar /><IoIosStar /><IoIosStar /><IoIosStar /><FaStarHalf /></div>
+<div className='mt-12  pl-14 pr-14  '>
+<img className='rounded-lg ' src="/src/assets/images/Feature img 2.png" alt="ti-shirt" />
+<h4 className='font-bold mt-2'>Skinny Fit Jeans</h4>
+<p className='flex flex-wrap text-yellow-300 mt-1 '><IoIosStar /><IoIosStar /><IoIosStar /><IoIosStar /><FaStarHalf /></p>
+<h1 className='mt-2 ml-1 font-satoshi text-1xl'>$121</h1>
 </div>
 
 
-<div className='flex flex-wrap gap-64 ml-10 font-satoshi text-1xl'>
-<h1 className='mt-2 ml-3'>$120</h1>
-<h1 className='mt-2 ml-12'>$122</h1>
-<h1 className='mt-2 ml-8'>$124</h1>
-<h1 className='mt-2 ml-7'>$12</h1>
+<div className='mt-12  pl-14 pr-14  '>
+<img className='rounded-lg ' src="/src/assets/images/Feature img 3.png" alt="ti-shirt" />
+<h4 className='font-bold mt-2'>Checkered Shirt</h4>
+<p className='flex flex-wrap text-yellow-300 mt-1 '><IoIosStar /><IoIosStar /><IoIosStar /><IoIosStar /><FaStarHalf /></p>
+<h1 className='mt-2 ml-1 font-satoshi text-1xl'>$122</h1>
 </div>
 
 
 
-<div className="flex items-center justify-center mt-8">
+<div className='mt-12  pl-14 pr-14  '>
+<img className='rounded-lg ' src="/src/assets/images/Feature img 4.png" alt="ti-shirt" />
+<h4 className='font-bold mt-2'>Sleeve Striped T-shirt</h4>
+<p className='flex flex-wrap text-yellow-300 mt-1 '><IoIosStar /><IoIosStar /><IoIosStar /><IoIosStar /><FaStarHalf /></p>
+<h1 className='mt-2 ml-1 font-satoshi text-1xl'>$123</h1>
+</div>
+</div>
+
+<div className="flex items-center justify-center mt-12">
   <button className="pl-9 pr-9 p-2 border border-gray-400 rounded-xl font-semibold">
     View All
   </button>
 </div><br /><br /><br />
 <hr />
 <br /><br />
+
+
+
+{/* {Top SElling} */}
+
 <h1 className='text-4xl font-bold text-center'>TOP SELLING</h1>
 
 
 
 
+<div className='flex'>
 
-<div className='mt-12 flex pl-14 pr-14 gap-6 '>
-<img className='rounded-lg ' src="/src/assets/imges/Feature img 5.png" alt="ti-shirt" />
-<img className='rounded-lg ' src="/src/assets/imges/Feature img 6.png" alt="ti-shirt" />
-<img className='rounded-lg 'src="/src/assets/imges/Feature img 7.png" alt="ti-shirt" />
-<img className='rounded-lg 'src="/src/assets/imges/Feature img 8.png" alt="ti-shirt" />
-
-</div>
-
-
-<div className='flex gap-32 ml-14'>
+<div className='mt-12  pl-14 pr-14  '>
+<img className='rounded-lg ' src="/src/assets/images/Feature img 5.png" alt="ti-shirt" />
 <h4 className='font-bold mt-2'>T-Shirt with Tape Details</h4>
-<h4 className='font-bold mt-2 ml-5'>T-Shirt with Tape Details</h4>
-<h4 className='font-bold mt-2 ml-2'>T-Shirt with Tape Details</h4>
-<h4 className='font-bold mt-2 '>T-Shirt with Tape Details</h4>
+<p className='flex flex-wrap text-yellow-300 mt-1 '><IoIosStar /><IoIosStar /><IoIosStar /><IoIosStar /><FaStarHalf /></p>
+<h1 className='mt-2 ml-1 font-satoshi text-1xl'>$124</h1>
 </div>
 
 
-<div className='flex gap-60 ml-6'>
-
-<div className='flex flex-wrap text-yellow-300 mt-1 ml-7'><IoIosStar /><IoIosStar /><IoIosStar /><IoIosStar /><FaStarHalf /><p></p></div>
-<div className='flex flex-wrap text-yellow-300 mt-1 ml-4'><IoIosStar /><IoIosStar /><IoIosStar /><IoIosStar /><FaStarHalf /></div>
-<div className='flex flex-wrap text-yellow-300 mt-1 '><IoIosStar /><IoIosStar /><IoIosStar /><IoIosStar /><FaStarHalf /></div>
-
-<div className='flex flex-wrap text-yellow-300 mt-1 '><IoIosStar /><IoIosStar /><IoIosStar /><IoIosStar /><FaStarHalf /></div>
+<div className='mt-12  pl-14 pr-14  '>
+<img className='rounded-lg ' src="/src/assets/images/Feature img 6.png" alt="ti-shirt" />
+<h4 className='font-bold mt-2'>Skinny Fit Jeans</h4>
+<p className='flex flex-wrap text-yellow-300 mt-1 '><IoIosStar /><IoIosStar /><IoIosStar /><IoIosStar /><FaStarHalf /></p>
+<h1 className='mt-2 ml-1 font-satoshi text-1xl'>$125</h1>
 </div>
 
 
-<div className='flex flex-wrap gap-64 ml-10 font-satoshi text-1xl'>
-<h1 className='mt-2 ml-3'>$121</h1>
-<h1 className='mt-2 ml-12'>$123</h1>
-<h1 className='mt-2 ml-8'>$125</h1>
-<h1 className='mt-2 ml-7'>$127</h1>
+<div className='mt-12  pl-14 pr-14  '>
+<img className='rounded-lg ' src="/src/assets/images/Feature img 7.png" alt="ti-shirt" />
+<h4 className='font-bold mt-2'>Checkered Shirt</h4>
+<p className='flex flex-wrap text-yellow-300 mt-1 '><IoIosStar /><IoIosStar /><IoIosStar /><IoIosStar /><FaStarHalf /></p>
+<h1 className='mt-2 ml-1 font-satoshi text-1xl'>$126</h1>
 </div>
 
 
 
-<div className="flex items-center justify-center mt-10">
+<div className='mt-12  pl-14 pr-14  '>
+<img className='rounded-lg ' src="/src/assets/images/Feature img 8.png" alt="ti-shirt" />
+<h4 className='font-bold mt-2'>Sleeve Striped T-shirt</h4>
+<p className='flex flex-wrap text-yellow-300 mt-1 '><IoIosStar /><IoIosStar /><IoIosStar /><IoIosStar /><FaStarHalf /></p>
+<h1 className='mt-2 ml-1 font-satoshi text-1xl'>$127</h1>
+</div>
+</div>
+
+
+<div className="flex items-center justify-center mt-12">
   <button className="pl-9 pr-9 p-2 border border-gray-400 rounded-xl font-semibold">
     View All
   </button>
-</div><br /><br />
-
+</div>
 </div>
 
-    
+
+
+
 
   )
 }
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function HeroSection() {
   return (
-    <div className="bg-[url('../assets/imges/rectangle2.png')] bg-no-repeat bg-cover w-full h-screen">
+    <div className="bg-[url('/src/assets/images/rectangle2.png')] bg-no-repeat bg-cover w-full h-screen">
      
      <h1 className='font-integral font-extrabold text-6xl pl-24 pt-24 w-577'>FIND CLOTHES <br />THAT MATCHES <br /> YOUR STYLE</h1>
       <p className='text-customGrayDark pt-4 pl-28 text-sm'>Browse through our diverse range of meticulously crafted garments, designed <br /> to bring out your individuality and cater to your sense of style.</p>
@@ -23,7 +23,7 @@ export default function HeroSection() {
        </div>  
   </div>
   <div className='bg-black max-w-full mt-20  flex flex-wrap gap-24 p-8  '>
-<img className='ml-10' src="/src/assets/imges/hero-vector-1.png" alt="VERSAGE" /> <img src="/src/assets/imges/hero-vector-2.png" alt="ZARA" /><img src="/src/assets/imges/hero-vector-3.png" alt="GUCCI" /><img src="/src/assets/imges/hero-vector-4.png" alt="PRADA" /><img src="/src/assets/imges/hero-vector-5.png" alt="Calvin Klein" />
+<img className='ml-10' src="/src/assets/images/hero-vector-1.png" alt="VERSAGE" /> <img src="/src/assets/images/hero-vector-2.png" alt="ZARA" /><img src="/src/assets/images/hero-vector-3.png" alt="GUCCI" /><img src="/src/assets/images/hero-vector-4.png" alt="PRADA" /><img src="/src/assets/images/hero-vector-5.png" alt="Calvin Klein" />
 </div>
 
       </div>
